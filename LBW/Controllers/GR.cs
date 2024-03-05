289102eb4bc60958d142b63a4cdb5eec3d0b506c
Fabrizio <fabriziosebastianbusiness@gmail.com>
2024-03-04 19:06:59 -0500

    beggin

---
+++ LBW/Controllers/GR.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LBW.Controllers
+{
+    public class GRController : Controller
+    {
+        public IActionResult Analisis()
+        {
+            @ViewBag.analisis = "active";
+            return View();
+        }
+        public IActionResult Autorizar()
+        {
+            @ViewBag.autorizar = "active";
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ LBW/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+var connStr = builder.Configuration.GetConnectionString("CadenaSQL");
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+
+// Add services to the container.
+builder.Services
+    .AddDbContext<LBW.Models.Entity.LbwContext>(options =>
+    { object value = options.UseSqlServer(connStr); })
+    .AddControllersWithViews()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseSession();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+ 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Inicio}/{action=Bienvenida}/{id?}");
+
+app.Run();
